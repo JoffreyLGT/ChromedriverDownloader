9c8e7e0ec494c06587a788d95dba9d84cd8f4a5d
Joffrey Lagut <j.lagut@live.fr>
2022-11-18 16:39:18 +0100

    First iteration

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# Chromedriver Downloader
+
+Sick of having to update your chromedriver each time there is a new version of Google Chrome?
+This lib is for you.
+
+## 🎯 Goal
+- Download a supported chromedriver for your version of Google Chrome
+
+## ⚙ Features
+- Detect your installed Google Chrome version and download the latest chromedriver supporting it.
+- Allow you to force the download of a new driver.
+- Support ChromeOptions.
+
+## 📗 How to use it?
+Simply call the function **GetDriver()** to get your ChromeDriver.
+Bellow is a sample code using it:
+```C#
+Console.WriteLine("Chromedriver tester started");
+ChromeOptions options = new ChromeOptions();
+options.AddArgument("--log-level=3"); // Fatal errors only
+ChromeDriver driver = await ChromedriverDownloader.GetDriver(options);
+Console.WriteLine("Google Chrome should be opened. Press a key to quit.");
+Console.ReadLine();
+driver.Quit();
+```
---
+++ Utilities.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace Chromedriver_Downloader
+{
+    internal static class Utilities
+    {
+        private const string CHROMEDRIVER_LATEST_URL = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_";
+        private const string CHROMEDRIVER_DOWNLOAD_URL = "https://chromedriver.storage.googleapis.com/{version}/chromedriver_win32.zip";
+
+        /// <summary>
+        /// Return the url to get the latest chromedriver url for Chrome version provided.
+        /// </summary>
+        /// <param name="version">Chrome version to look for a compatible driver</param>
+        /// <returns></returns>
+        internal static string GetChromedriverLatestUrl(string version)
+        {
+            return CHROMEDRIVER_LATEST_URL + version;
+        }
+
+        internal static string GetChromedriverDownloadUrl(string version)
+        {
+            return CHROMEDRIVER_DOWNLOAD_URL.Replace("{version}", version.ToString());
+        }
+
+        /// <summary>
+        /// Get the path to the Google Chrome installation.
+        /// </summary>
+        /// <returns>a full path to chrome.exe</returns>
+        /// <exception cref="FileNotFoundException">when chrome.exe is not found in C:\Program Files\Google\Chrome\Application\ or C:\Program Files (x86)\Google\Chrome\Application\</exception>
+        internal static string GetChromePath()
+        {
+            string fromProgramFile = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+            string fromProgramFile86 = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+
+            if (File.Exists(fromProgramFile))
+            {
+                return fromProgramFile;
+            }
+            else if(File.Exists(fromProgramFile86))
+            {
+                return fromProgramFile86;
+            }
+            throw new FileNotFoundException("Chrome installation not found");
+        }
+
+        /// <summary>
+        /// Return the major part of Google Chrome version.
+        /// </summary>
+        /// <param name="chromePath">to chrome.exe</param>
+        /// <returns>chrome major version number</returns>
+        /// <example>for example, 107 for 107.0.3.432</example>
+        internal static int GetChromeVersion(string chromePath)
+        {
+            return FileVersionInfo.GetVersionInfo(chromePath).FileMajorPart;
+        }
+    }
+}
